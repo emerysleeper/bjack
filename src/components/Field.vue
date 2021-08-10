7135c192cfbd0f7440c476739228baa254c83448
atmenstill <atmenstill@gmail.com>
2021-08-10 17:46:17 +0300

    All the main methods are done

--- src/components/Field.vue
+++ src/components/Field.vue
@@ -2,11 +2,34 @@
   <div>
     <p>В руках игрока: {{ playerCards }}</p>
     <p>В руках дилера: {{ dealerCards }}</p>
-    <div @click="giveCard">
+    <p>Ставка игрока: {{ stake }}</p>
+    <p>Деньги игрока: {{ money }}</p>
+    <div @click="giveCardToPlayer">
       <p>Выдать карту игроку</p>
     </div>
-    <div @click="giveCard">
+    <div @click="giveCardToDealer">
       <p>Выдать карту дилеру</p>
+    </div>
+    <div @click="clearPlayerHands">
+      <p>Очистить руки игрока</p>
+    </div>
+    <div @click="clearDealerHands">
+      <p>Очистить руки дилера</p>
+    </div>
+    <div @click="clearBoth">
+      <p>Очистить всех</p>
+    </div>
+    <div @click="setStake">
+      <p>Поставить 100</p>
+    </div>
+    <div @click="win3to2">
+      <p>3 к 2</p>
+    </div>
+    <div @click="win1to1">
+      <p>1 к 1</p>
+    </div>
+    <div @click="loseStake">
+      <p>Проигрыш</p>
     </div>
   </div>
 </template>
@@ -23,13 +46,28 @@ export default {
   },
   computed: {
     ...mapGetters({
-      playerCards: 'getPlayerCards'
+      playerCards: 'getPlayerCards',
+      dealerCards: 'getDealerCards',
+      stake: 'getStake',
+      money: 'getPlayerMoney'
     })
   },
   methods: {
     ...mapActions({
-      giveCard: 'giveCard'
+      giveCardToPlayer: 'giveCardToPlayer',
+      giveCardToDealer: 'giveCardToDealer',
+      start: 'startGame',
+      clearPlayerHands: 'clearPlayerHands',
+      clearDealerHands: 'clearDealerHands',
+      clearBoth: 'clearBoth',
+      setStake: 'setStake',
+      win3to2: 'win3to2',
+      win1to1: 'win1to1',
+      loseStake: 'loseStake'
     })
+  },
+  created() {
+    this.start()
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -7,23 +7,27 @@ export default new Vuex.Store({
   state: {
     gameStarted: false,
     //max - 8 or infinite
-    decksAmount: null,
+    decksAmount: 1,
     deck: null,
     dealerCards: [],
     playerCards: [],
     playerStakes: null,
-    nextCard: null
+    nextCard: null,
+    stake: 0,
+    playerMoney: 5000
   },
   getters: {
     getPlayerCards: state => state.playerCards,
-    getDealerCards: state => state.dealerCards
+    getDealerCards: state => state.dealerCards,
+    getStake: state => state.stake,
+    getPlayerMoney: state => state.playerMoney
   },
   mutations: {
     GIVE_PLAYER_CARD (state, payload) {
       state.playerCards.push(payload)
     },
     GIVE_DEALER_CARD (state, payload) {
-      state.playerCards.push(payload)
+      state.dealerCards.push(payload)
     },
     CLEAR_CARDS_PLAYER (state) {
       state.playerCards = []
@@ -33,15 +37,60 @@ export default new Vuex.Store({
     },
     ASSIGN_NEXT_CARD (state, payload) {
       state.nextCard = payload
+    },
+    POPULATE_DECK (state, payload) {
+      state.deck = payload
+    },
+    SET_DECKS_AMOUNT (state, payload) {
+      state.decksAmount = payload
+    },
+    DELETE_CARD_FROM_DECK (state, payload) {
+      state.deck.splice(payload, 1)
+    },
+    CLEAR_PLAYER (state) {
+      state.playerCards = []
+    },
+    CLEAR_DEALER (state) {
+      state.dealerCards = []
+    },
+    ASSIGN_STAKE (state, payload) {
+      state.stake = payload
+    },
+    ASSIGN_MONEY (state, payload) {
+      state.playerMoney = payload
     }
   },
   actions: {
-    startGame() {
-
+    startGame({ dispatch, commit }) {
+      commit('SET_DECKS_AMOUNT', 1)
+      dispatch('populateDeck')
     },
-    giveCard ({ dispatch, commit, state }) {
-      dispatch('infiniteDeckRandomCard')
+    giveCardToPlayer ({ dispatch, commit, state }) {
+      dispatch('populateDeck')
+      dispatch('randomCard')
       commit('GIVE_PLAYER_CARD', state.nextCard)
+    },
+    giveCardToDealer ({ dispatch, commit, state }) {
+      dispatch('randomCard')
+      commit('GIVE_DEALER_CARD', state.nextCard)
+    },
+    populateDeck ({ commit, state }) {
+      const decks = []
+      for (let nums = 0; nums < state.decksAmount; nums++) {
+        for (let i = 0; i < 4; i++) {
+          for (let j = 0; j < 13; j++) {
+            decks.push({suit: i, value: j})
+          }
+        }
+      }
+      commit('POPULATE_DECK', decks)
+    },
+    // Gives random card from the populated deck and deletes
+    // the card from the deck
+    randomCard({ commit, state }) {
+      const cardNumber = Math.floor(Math.random()*state.deck.length)
+      commit('ASSIGN_NEXT_CARD', state.deck[cardNumber])
+      commit('DELETE_CARD_FROM_DECK', cardNumber)
     },
     //Gives random card in case we play game with infinite decks
     infiniteDeckRandomCard({ commit }) {
@@ -52,6 +101,37 @@ export default new Vuex.Store({
         const cardValue = Math.floor(Math.random()*13)
         commit('ASSIGN_NEXT_CARD', [suit, cardValue])
       // }
+    },
+    clearPlayerHands({ commit }) {
+      commit('CLEAR_PLAYER')
+    },
+    clearDealerHands({ commit }) {
+      commit('CLEAR_DEALER')
+    },
+    clearBoth({ commit }) {
+      commit('CLEAR_PLAYER')
+      commit('CLEAR_DEALER')
+    },
+    setStake({ commit, state }) {
+      const stake = 100
+      commit('ASSIGN_STAKE', stake)
+      const newPlayerMoney = state.playerMoney - stake
+      commit('ASSIGN_MONEY', newPlayerMoney)
+    },
+    win3to2({ commit, state }) {
+      const wonStake = state.stake * 2.5
+      const newPlayerMoney = state.playerMoney + wonStake
+      commit('ASSIGN_STAKE', 0)
+      commit('ASSIGN_MONEY', newPlayerMoney)
+    },
+    win1to1({ commit, state }) {
+      const wonStake = state.stake * 2
+      const newPlayerMoney = state.playerMoney + wonStake
+      commit('ASSIGN_STAKE', 0)
+      commit('ASSIGN_MONEY', newPlayerMoney)
+    },
+    loseStake({ commit }) {
+      commit('ASSIGN_STAKE', 0)
     }
   },
   modules: {
