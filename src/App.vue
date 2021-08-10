6524e72b089c0c78189a280e341d3b7f89fd112b
Eschaton-Futuro\Eschaton <emerysleeper@gmail.com>
2021-08-10 08:46:15 +0300

    Initial beginnings

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+    <Field />
+  </div>
+</template>
+
+<script>
+import Field from "@/components/Field";
+export default {
+  name: 'App',
+  components: {
+    Field
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ src/components/Field.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <p>Вам выдана {{ curCard }}</p>
+    <div @click="giveCard">
+      <p>Выдать карту</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Field',
+  data () {
+    return {
+      allCards: null,
+      curCard: null
+    }
+  },
+  methods: {
+    giveCard () {
+      this.curCard = this.randomCard()
+    },
+    randomCard() {
+      if(Math.floor(Math.random() * 54) < 2) {
+        return 'Joker'
+      } else {
+        const suit = Math.floor(Math.random()*4)
+        const cardValue = Math.floor(Math.random()*13)
+        return [suit, cardValue]
+
+      }
+    }
+  }
+  // created () {
+  //   const fullDeck = []
+  //   for (let suit in 4) {
+  //     for (let val in )
+  //   }
+  // }
+}
+</script>
